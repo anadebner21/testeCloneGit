904c2e18d55468f64e79ea7dae685a0710a6ec0a
Samantha <samanthagolim@gmail.com>
2020-02-14 06:21:56 -0300

    fiz o que o prof pediu

--- teste.c
+++ teste.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 
 int main(){
+	int idade; 
+	
+
+	printf("Digite sua idade");
+	scanf("%d", &idade);
+
+	if (idade > 0){
+		idade -= 1 ;
+	}
+
+	printf ("sua idade subtraida por 1 é: %d", idade);
 
 
     return 0;
